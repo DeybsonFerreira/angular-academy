7a612f26eb58637b0c43eb25f41b31d03a03da55
Deybson Ferreira <deybson.ferreira@iatec.com>
2023-05-29 18:28:40 -0300

    tela login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { FormsModule } from '@angular/forms';
     ContactComponent,
     NavbarComponent,
     FooterComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
